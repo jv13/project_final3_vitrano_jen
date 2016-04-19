feaa7854df4677d0377e80e89dff9dbd1fcc4745
Jen Vitrano <vitranojen@gmail.com>
2016-04-19 10:55:52 -0400

    Add comments to js and update spacing in slippery CSS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+// jQuery for header hamburger nav
 $( function() {
 		$( '#menu' ).slicknav();
 } );
 
+// jQuery homepage slider
 jQuery( document ).ready( function() {
   jQuery( '.carousel' ).slippry();
 } );
