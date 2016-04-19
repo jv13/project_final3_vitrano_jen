71d5a48b81e49064e87e0d67b1eafb0e7fd827af
Jen Vitrano <vitranojen@gmail.com>
2016-04-19 12:42:53 -0400

    Remove p titles add add event listener js function
    The js function will listen for a click on the images and then add the name, description, and price of the image

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ $( function() {
 jQuery( document ).ready( function() {
   jQuery( '.carousel' ).slippry();
 } );
+
+var appOne = document.getElementById( 'chbrd' );
+
+appOne.addEventListener( 'click', function() {
+    var myApp = document.createElement( 'article' );
+		myApp.innerHTML = '<p>cheesy bread</p><p>New to our app menus</p><p>$2.99</p>';
+		document.getElementsByClassName( 'menuitems' )[ 0 ].appendChild( myApp );
+} );
